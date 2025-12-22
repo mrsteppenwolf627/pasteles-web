1b401cdb4c4b2b823520bd0dc64136e2e0eb5c9f
AItor <aitoralmu21@gmail.com>
2025-12-22 12:21:25 +0100

    Configuracion Sanity guardada de verdad

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,21 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
+import sanity from "@sanity/astro"; // <--- ESTO FALTABA
 
 // https://astro.build/config
 export default defineConfig({
+  // 1. Aquí conectamos la base de datos
+  integrations: [
+    sanity({
+      projectId: "y7m52g15", // <--- ¡PEGA TU ID AQUÍ!
+      dataset: "production",
+      useCdn: true,
+    }),
+  ],
+
+  // 2. Aquí configuramos el diseño
   vite: {
     plugins: [tailwindcss()]
   }
-});// Forzando actualizacion
+});
