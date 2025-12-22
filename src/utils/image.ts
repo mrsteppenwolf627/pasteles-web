4b4a8cd4a3da3102bd6531e053358c686f984034
AItor <aitoralmu21@gmail.com>
2025-12-22 13:12:05 +0100

    Guardando trabajo por hoy

--- src/utils/image.ts
+++ src/utils/image.ts
@@ -1,8 +1,9 @@
 import imageUrlBuilder from '@sanity/image-url';
-import { sanityClient } from 'sanity:client';
+import { sanityClient } from '../sanity';
 
 const builder = imageUrlBuilder(sanityClient);
 
-export function urlFor(source) {
+// Hemos añadido ": any" para que no de error
+export function urlFor(source: any) {
   return builder.image(source);
-}// Despierta Git
+}
