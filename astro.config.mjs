ed78f6692d46b4936006afaeaa1d742ee4b9b9f4
Aitor <aitoralmu21@gmail.com>
2025-12-26 18:08:22 +0100

    Añadida verificacion Google

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,21 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
-import sanity from "@sanity/astro"; 
+import tailwind from '@astrojs/tailwind';
+import react from '@astrojs/react';
+import sanity from '@sanity/astro';
 
 // https://astro.build/config
 export default defineConfig({
-  // 1. Aquí conectamos la base de datos
+  // TU DOMINIO OFICIAL (Esto es lo clave para Google)
+  site: 'https://manolipastelesdepañales.com',
+
   integrations: [
+    tailwind(),
+    react(),
     sanity({
-      projectId: "y7m52g15", 
-      dataset: "global", // <--- ¡AQUÍ ESTABA EL ERROR! (Cambiado de production a global)
-      useCdn: false, // Ponemos false para que se actualice al instante (sin caché)
+      projectId: 'y7m52g15', // Tu ID de proyecto
+      dataset: 'production',
+      useCdn: true, // Usar la red rápida (gratis)
+      apiVersion: '2024-03-20',
     }),
   ],
-
-  // 2. Aquí configuramos el diseño
-  vite: {
-    plugins: [tailwindcss()]
-  }
 });
