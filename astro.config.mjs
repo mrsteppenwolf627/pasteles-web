3f1640a0e5d6bd83ac614eb9d1202d4fb52562eb
Aitor <aitoralmu21@gmail.com>
2025-12-28 16:27:16 +0100

    Añadido Sitemap automatico

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,17 +3,15 @@ import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 import sanity from '@sanity/astro';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://manolipastelesdepañales.com',
-  integrations: [
-    tailwind(),
-    react(),
-    sanity({
-      projectId: 'y7m52g15',
-      dataset: 'global', // <--- ¡AQUÍ ESTABA EL DUENDE! 😈 Antes ponía 'production'
-      useCdn: true,
-      apiVersion: '2024-03-20',
-    }),
-  ],
+  integrations: [tailwind(), react(), sanity({
+    projectId: 'y7m52g15',
+    dataset: 'global', // <--- ¡AQUÍ ESTABA EL DUENDE! 😈 Antes ponía 'production'
+    useCdn: true,
+    apiVersion: '2024-03-20',
+  }), sitemap()],
 });
