2b2f08a85ce8a6a066d124dd8b90afe49418a4bf
Aitor <aitoralmu21@gmail.com>
2025-12-29 12:35:40 +0100

    Optimizar imagenes a webp y 500px

---
+++ src/lib/urlFor.ts
@@ -0,0 +1,9 @@
+import imageUrlBuilder from '@sanity/image-url';
+import { sanityClient } from 'sanity:client';
+
+const builder = imageUrlBuilder(sanityClient);
+
+export function urlFor(source: any) {
+  return builder.image(source);
+}
+
